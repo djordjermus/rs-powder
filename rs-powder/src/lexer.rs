c3e470d5de166b1256742d70fdcc95e877da34a3
djordjermus <djordje.rmus2000@google.com>
2023-04-02 20:36:35 +0200

    rename lexer functions to tokenizers

--- rs-powder/src/lexer.rs
+++ rs-powder/src/lexer.rs
@@ -3,24 +3,26 @@ type Tokenizer<T> = fn(text: &str) -> Option<(T, usize)>;
 type Skip = fn(text: &str) -> usize;
 pub struct Lexer<T>
 {
-	lexers: std::vec::Vec<Tokenizer<T>>,
+	tokenizers: std::vec::Vec<Tokenizer<T>>,
 	skipper: Skip
 }
 impl<T> Lexer<T> {
-	pub fn new(lexers: &[Tokenizer<T>], skipper: Skip) -> Self {
-		return Self {lexers: std::vec::Vec::<Tokenizer<T>>::from(lexers), skipper: skipper };
+	pub fn new(tokenizers: &[Tokenizer<T>], skipper: Skip) -> Self {
+		return Self {tokenizers: std::vec::Vec::<Tokenizer<T>>::from(tokenizers), skipper: skipper };
 	}
 
-	pub fn add(&mut self, lexer: Tokenizer<T>) -> () {
-		self.lexers.push(lexer);
+	// Adds tokenizers to the lexer.
+	// Added tokenizer will be executed after every previously added tokenizer fails. 
+	pub fn add(&mut self, tokenizer: Tokenizer<T>) -> () {
+		self.tokenizers.push(tokenizer);
 	}
 
-	// Tries to consume starting characters, in order to generate a token
-	// Will not attempt to skip characters
-	// Returns optionally token and number of consumed characters, if successful
+	// Tries to consume starting characters, in order to generate a token.
+	// Will not attempt to skip characters.
+	// Returns optionally token and number of consumed characters, if successful.
 	pub fn tokenize_skipless(&self, text: &str) -> Option<(T, usize)>
 	{
-		for lexer in &self.lexers {
+		for lexer in &self.tokenizers {
 			match lexer(text) {
 				Some((token, consumed)) => return Some((token, consumed)),
 				_ => continue
@@ -29,9 +31,9 @@ impl<T> Lexer<T> {
 		return None;
 	}
 
-	// Tries to consume starting characters, in order to generate a token
-	// Will attempt to skip characters before trying to generate a token
-	// Returns optionally token and number of consumed characters, if successful
+	// Tries to consume starting characters, in order to generate a token.
+	// Will attempt to skip characters before trying to generate a token.
+	// Returns optionally token and number of consumed characters, if successful.
 	pub fn tokenize(&self, text: &str) -> Option<(T, usize, usize)> {
 		let skip = (self.skipper)(text);
 		match self.tokenize_skipless(&text[skip..]) {
@@ -40,6 +42,9 @@ impl<T> Lexer<T> {
 		}
 	}
 
+	// Generates as many tokens as possible, until running out of text, or if every tokenizer fails.
+	// Generated tokens are pushed into the provided list.
+	// Returns number of consumed characters.
 	pub fn tokenize_into_list(&self, text: &str, out: &mut std::vec::Vec<(T, usize, usize)>) -> usize {
 		let mut acc: usize = 0usize;
 		while acc < text.len() {
